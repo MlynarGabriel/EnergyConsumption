7a59791f59922a2fc263837464fb366f4c5411a2
Gabriel Mlynar <gabriel.mlynar@gmx.at>
2025-03-26 13:49:46 +0100

    first commit

---
+++ src/test/java/org/example/energyconsumption/EnergyConsumptionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.energyconsumption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyConsumptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
