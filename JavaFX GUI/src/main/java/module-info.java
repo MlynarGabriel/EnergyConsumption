822b9417e4d3f9bf5f8f68eee6d4f960d6435da6
Gabriel Mlynar <gabriel.mlynar@gmx.at>
2025-04-10 13:33:15 +0200

    JavaFX UI added

--- JavaFX GUI/src/main/java/module-info.java
+++ JavaFX GUI/src/main/java/module-info.java
@@ -1,8 +1,7 @@
 module org.example.ui {
+    requires java.net.http;
     requires javafx.controls;
     requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
 
     opens org.example.ui to javafx.fxml;
     exports org.example.ui;
--- JavaFX GUI/src/main/java/org/example/ui/HelloController.java
+++ JavaFX GUI/src/main/java/org/example/ui/HelloController.java
@@ -1,14 +1,52 @@
 package org.example.ui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+
+import java.time.LocalDate;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    @FXML private Label poolUsageLabel;
+    @FXML private Label gridPortionLabel;
+
+    @FXML private DatePicker startDate;
+    @FXML private DatePicker endDate;
+    @FXML private ComboBox<String> startTime;
+    @FXML private ComboBox<String> endTime;
+
+    @FXML private Label communityProduced;
+    @FXML private Label communityUsed;
+    @FXML private Label gridUsed;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+        // Zeitoptionen vorbereiten
+        for (int h = 0; h < 24; h++) {
+            String time = String.format("%02d:00", h);
+            startTime.getItems().add(time);
+            endTime.getItems().add(time);
+        }
+
+        startDate.setValue(LocalDate.now());
+        endDate.setValue(LocalDate.now());
+        startTime.setValue("14:00");
+        endTime.setValue("14:00");
+
+        refreshData();
+    }
+
+    @FXML
+    public void refreshData() {
+        poolUsageLabel.setText("Hier mit Controller Klasse arbeiten");
+        gridPortionLabel.setText("Hier mit Controller Klasse arbeiten");
+    }
+
+    @FXML
+    public void showData() {
+        // Daten anzeigen (Simuliert)
+        communityProduced.setText("Community produced: Hier mit Controller Klasse arbeiten");
+        communityUsed.setText("Community used: Hier mit Controller Klasse arbeiten");
+        gridUsed.setText("Grid used: Hier mit Controller Klasse arbeiten");
     }
 }
