1ac1733df0dc86245a4da400c67e6fabfe744a8f
anhbu <wi23b147@technikum-wien.at>
2025-06-17 19:16:40 +0200

    Service Usage

---
+++ Service-Usage/src/main/java/org/example/serviceusage/ServiceUsageApplication.java
@@ -0,0 +1,13 @@
+package org.example.serviceusage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceUsageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceUsageApplication.class, args);
+    }
+
+}
